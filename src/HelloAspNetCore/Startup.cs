00fe9b13c75f9ed347d52e8bb333be3e0add3485
unknown <xiaoxm_001@163.com>
2016-06-22 20:15:50 +0800

    hello aspnet core

---
+++ src/HelloAspNetCore/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloAspNetCore
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync("Hello from ASP.NET Core!");
+            });
+        }
+    }
+}
